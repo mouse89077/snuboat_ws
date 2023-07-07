3a8a6a5fcaf267b301b440563775373833823656
Minsung Park <j6847110@naver.com>
2023-07-07 15:12:35 +0900

    Update lidar_converter.py

--- src/snuboat_nav/snuboat_nav/lidar_converter.py
+++ src/snuboat_nav/snuboat_nav/lidar_converter.py
@@ -6,7 +6,6 @@ from rclpy.node import Node
 from geometry_msgs.msg import Point
 from rclpy.qos import qos_profile_sensor_data
 from std_msgs.msg import Bool, Float64, Float64MultiArray
-from snuboat_msgs.msg import Obstacles
 from sensor_msgs import LaserScan
 import numpy as np
 from sklearn.cluster import DBSCAN
@@ -22,7 +21,11 @@ class Lidar_Converter(Node):
         
         self.lidar_scan_sub = self.create_subscription(LaserScan, '/scan', self.lidar_scan_callback, qos_profile_sensor_data)
         
-        self.obstacles_pub = self.create_publisher(Obstacles, '/obstacles', 1)
+        self.obs_labels_pub = self.create_publisher(Float64MultiArray, '/obs/labels', 1)
+        self.obs_r_pub = self.create_publisher(Float64MultiArray, '/obs/r', 1)
+        self.obs_phi_pub = self.create_publisher(Float64MultiArray, '/obs/phi', 1)
+        self.obs_x_pub = self.create_publisher(Float64MultiArray, '/obs/x', 1)
+        self.obs_y_pub = self.create_publisher(Float64MultiArray, '/obs/y', 1)
         
         self.obstacles_timer = self.create_timer(self.dt, self.pub_obstacles)
 
@@ -63,13 +66,23 @@ class Lidar_Converter(Node):
         
     # publish obstacle sinfo
     def pub_obstacles(self):
-        obs = Obstacles()
-        obs.labels.data = self.scan_labels
-        obs.r.data = np.transpose(self.polar_scan[:, 0])
-        obs.phi.data = np.transpose(self.polar_scan[:, 1])
-        obs.x.data = np.transpose(self.cart_scan[:, 0])
-        obs.y.data = np.transpose(self.cart_scan[:, 1])
-        self.obstacles_pub.publish(obs)
+        obs_labels = Float64MultiArray()
+        obs_labels.data = self.scan_labels.tolist()
+        obs_r = Float64MultiArray()
+        obs_r.data = np.transpose(self.polar_scan[:, 0]).tolist()
+        obs_phi = Float64MultiArray()
+        obs_phi.data = np.transpose(self.polar_scan[:, 1]).tolist()
+        obs_x = Float64MultiArray()
+        obs_x.data = np.transpose(self.cart_scan[:, 0]).tolist()
+        obs_y = Float64MultiArray()
+        obs_y.data = np.transpose(self.cart_scan[:, 1]).tolist()
+        
+        self.obs_labels_pub.publish(obs_labels)
+        self.obs_r_pub.publish(obs_r)
+        self.obs_phi_pub.publish(obs_phi)
+        self.obs_x_pub.publish(obs_x)
+        self.obs_y_pub.publish(obs_y)
+        
         
 def main(args=None):
     rclpy.init(args=args)
